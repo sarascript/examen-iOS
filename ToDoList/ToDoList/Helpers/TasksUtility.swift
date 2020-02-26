81add7fafd72e6c851473f87de39cdad3d7a8460
Sara Correas G <32343542+sarascript@users.noreply.github.com>
2020-02-26 12:25:14 +0100

    Feature "ToDoList" working

---
+++ ToDoList/ToDoList/Helpers/TasksUtility.swift
@@ -0,0 +1,30 @@
+
+import Foundation
+
+class TasksUtility {
+    
+    private static let key = "tasks"
+    
+    // archive
+    private static func archive(_ tasks: [[Task]]) -> NSData {
+        return NSKeyedArchiver.archivedData(withRootObject: tasks) as NSData
+    }
+    
+    // fetch
+    static func fetch() -> [[Task]]? {
+        guard let unarchivedData = UserDefaults.standard.object(forKey: key) as? Data else { return nil }
+        
+        return NSKeyedUnarchiver.unarchiveObject(with: unarchivedData) as? [[Task]]
+    }
+    
+    // save
+    static func save(_ tasks: [[Task]]) {
+        
+        // archive
+        let archivedTasks = archive(tasks)
+        
+        // set object for key
+        UserDefaults.standard.set(archivedTasks, forKey: key)
+        UserDefaults.standard.synchronize()
+    }
+}
